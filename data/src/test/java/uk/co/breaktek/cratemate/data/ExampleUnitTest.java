8c6d43a3e2e3be0b4a4c091cebded62bf7eac2f4
Chris Shotton <chris.shotton@student.com>
2016-04-29 12:07:38 +0100

    Moving root directory for CI

---
+++ data/src/test/java/uk/co/breaktek/cratemate/data/ExampleUnitTest.java
@@ -0,0 +1,20 @@
+package uk.co.breaktek.cratemate.data;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
